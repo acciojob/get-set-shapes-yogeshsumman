cae5fae17aff0c5996a12db027328f4df581fc1c
Yogesh Summan <yogeshsumman2001@gmail.com>
2025-04-13 18:05:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,12 +7,21 @@ class Rectangle {
   getArea() {
     return this.width * this.height;
   }
+
+  getPerimeter() {
+    return 2 * (this.width + this.height);
+  }
 }
 
 class Square extends Rectangle {
   constructor(side) {
     // Call parent constructor with equal width and height
     super(side, side);
+  }
+
+  // Override getPerimeter for more efficient calculation
+  getPerimeter() {
+    return 4 * this.width; // or 4 * this.height since they're equal
   }
 }
 
